c0f6dd73bdc4af4d03a4ba378848d868974cbb6c
ishaan <ishaaniitr@gmail.com>
2018-03-15 13:26:06 +0530

    added templates for qa and hotlinked it to other templates folder

--- qa/views.py
+++ qa/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponse
 
 def qa(request, uid_project):
 	project = Project.objects.get(uid=uid_project)
+	active = 'qa_menu'
 
-	return HttpResponse('hi new architecture')
+	return render(request, 'qa/home.html', {'project':project, 'active':active})
 
 # Create your views here.
