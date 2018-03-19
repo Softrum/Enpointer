507c8ec026ff38df8055e571ff16df84191c7011
ishaan <ishaaniitr@gmail.com>
2018-03-19 20:43:38 +0530

    add history and version model for wiki

--- wiki/models.py
+++ wiki/models.py
@@ -4,6 +4,7 @@ from django.db import models
 import uuid
 from dragonslayer.models import Project
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,17 +16,38 @@ class Category(models.Model):
 
 
 
-class Page(models.Model):
+
+class Version(models.Model):
 	uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 	title = models.CharField(max_length=100, null = True, blank = True)
-	poject = models.ForeignKey(Project , null=True)
+	content = models.TextField(null=True, blank=True)
+	author = models.ForeignKey(User)
+	created = models.DateTimeField(null=True, blank=True)
+
+
+class Page(models.Model):
+	uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+	project = models.ForeignKey(Project , null=True)
 	category = models.ForeignKey(Category, null=True)
 	published = models.BooleanField(default=False)
-
-
-	content = models.TextField(null=True, blank=True)
+	current_version = models.ForeignKey(Version, null=True)
+	versions = models.ManyToManyField(Version, related_name='versions')
+	author = models.ForeignKey(User, related_name='pages')
 	created = models.DateTimeField(default=datetime.now, blank=True)
 	updated = models.DateTimeField(null=True, blank=True)
+
+
+
+
+
+
+class History(models.Model):
+	uid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+	message = models.CharField(max_length=500, null=True, blank=True)
+	page = models.ForeignKey(Page)
+	created =models.DateTimeField(default=datetime.now, blank=True)
+	project =  models.ForeignKey(Project , null=True, related_name='history')
+		
 
 
 
